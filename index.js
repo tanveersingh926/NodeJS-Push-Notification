1b899749bb89464fefa3bf8e2ed747ab026e45d8
tanveersingh926 <tanveer.singh926@gmail.com>
2018-06-04 15:07:52 +0800

    middleware added, route configured with subscription

--- index.js
+++ index.js
@@ -5,13 +5,30 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "client")));
+app.use(bodyParser.json())
+
 const publicVapidKey = process.env.Public_Key;
 const privateVapidKey = process.env.Private_Key;
 
 webPush.setVapidDetails('mailto:tanveer@tanveer.com', publicVapidKey, privateVapidKey)
 
+
+// Subscribe POST route
 app.post('/subscribe', (req, res)=>{
-    res.status(200).json({message:"Thanks"})
+
+    // Get Subsciption Object
+    const subsciption = req.body;
+    
+    // Send 201 - resource created
+    res.status(201).json({message:"resource created"})
+
+    // Create Payload
+    const payload = JSON.stringify({title:"Push Test"})
+
+    // Pass object into sendNotification
+    webPush.sendNotification(subsciption, payload)
+    .catch(err => console.log(err))
 })
 
-app.listen(3002);
+app.listen(3002, ()=> { console.log("Server started at 3002") });
