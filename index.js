0a6042f299d4435f9fd32877250caedd8c3a04b2
tanveersingh926 <tanveer.singh926@gmail.com>
2018-06-04 14:58:54 +0800

    configured nodemon, express and webpush vapid keys

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+
+const publicVapidKey = process.env.Public_Key;
+const privateVapidKey = process.env.Private_Key;
+
+webPush.setVapidDetails('mailto:tanveer@tanveer.com', publicVapidKey, privateVapidKey)
+
+app.post('/subscribe', (req, res)=>{
+    res.status(200).json({message:"Thanks"})
+})
+
+app.listen(3002);
